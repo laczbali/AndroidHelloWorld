cc6e41b251912ba583a8d077c798c3a2bf786cb5
LaczkóBalázs <lab@frankly.dk>
2023-11-01 14:33:59 +0100

    minor changes

--- app/src/main/java/com/example/androidhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/androidhelloworld/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.androidhelloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidhelloworld.ui.theme.AndroidHelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.Red) {
+        Text(
             text = "Hello $name!",
-            modifier = modifier
-    )
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
